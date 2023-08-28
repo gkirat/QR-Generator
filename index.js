773eb10dd6caf6a7d1fe3da3f1bb7b68e58b175a
gkirat <gkirat155@gmail.com>
2023-08-28 09:55:13 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      message: "type your url",
+      //   prefix:"https://",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qrImage.png"));
+
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("your data is saved");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
